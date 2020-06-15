1ebedb9523c423f64e325fdbfd2382a310634022
Quecia Alves <marquesquecia23@gmail.com>
2020-06-15 20:03:41 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/queciaalves/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.queciaalves.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.queciaalves.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/queciaalves/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.queciaalves.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.queciaalves.workshopmongo.domain.User;
+import com.queciaalves.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();		
+	}
+}
