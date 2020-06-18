5910620997b9e37f710d05b858d72fa08ece9445
Quecia Alves <marquesquecia23@gmail.com>
2020-06-18 14:11:21 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/queciaalves/workshopmongo/resources/util/URL.java
@@ -0,0 +1,15 @@
+package com.queciaalves.workshopmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+
+	public static String decodeParam(String text) {
+		try {
+			return URLDecoder.decode(text, "UFT-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
